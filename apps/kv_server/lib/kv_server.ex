1758401af5a3d297b9c49de846598d9525e90b0b
AlexKVal <alexkval@gmail.com>
2016-07-04 21:14:27 +0300

    add echo server

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -17,4 +17,40 @@ defmodule KVServer do
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  require Logger
+
+  def accept(port) do
+    # `:binary` - data as binaries (instead of lists)
+    # `packet: :line` - data line by line
+    # `active: false` - blocks on `:gen_tcp.recv/2` until data is available
+    # `reuseaddr: true` - allows to reuse the addr if the listener crashes
+    {:ok, socket} = :gen_tcp.listen(port,
+      [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info "Accepting connections on port #{port}"
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    serve(client)
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
+  end
 end
