eaef35f56a48aba838059c09fe2af7fe8be8c1e0
AlexKVal <alexkval@gmail.com>
2016-07-05 17:47:33 +0300

    make tests green again

--- apps/kv_server/lib/kv_server/command.ex
+++ apps/kv_server/lib/kv_server/command.ex
@@ -31,8 +31,11 @@ defmodule KVServer.Command do
   """
   def parse(line) do
     case String.split(line) do
-      ["CREATE", bucket] ->
-        {:ok, {:create, bucket}}
+      ["CREATE", bucket] -> {:ok, {:create, bucket}}
+      ["GET", bucket, key] -> {:ok, {:get, bucket, key}}
+      ["PUT", bucket, key, value] -> {:ok, {:put, bucket, key, value}}
+      ["DELETE", bucket, key] -> {:ok, {:delete, bucket, key}}
+      _ -> {:error, :unknown_command}
     end
   end
 end
